4ae6956de08f2d92983070401c18ac168b714ae3
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-09-26 16:56:21 -0600

    Creado el AFN

--- Proyecto1/src/Lib.hs
+++ Proyecto1/src/Lib.hs
@@ -1,4 +1,5 @@
 import AFD.AFD
+import AFN.AFN
 
 afdParDeAs = AFD
         ["q0", "q1"]
@@ -11,3 +12,15 @@ afdParDeAs = AFD
         ["q0"]
       
   
+miAFN = AFN
+  ["q0", "q1", "q2"]           -- Estados
+  ['a', 'b']                   -- Alfabeto
+  "q0"                         -- Estado Inicial
+  [ ("q0", 'a', ["q0", "q1"])   -- Transiciones:
+  , ("q0", 'b', ["q0"])         -- Si estoy en q0 y veo una 'b', me quedo en q0.
+  , ("q1", 'a', ["q1"])         -- Si estoy en q1 y veo otra 'a', reinicio la cuenta de 'a' quedándome en q1.
+  , ("q1", 'b', ["q2"])         -- Si estoy en q1 y veo una 'b', completo "ab" y voy a q2.
+  , ("q2", 'a', ["q1"])         -- Si ya terminé y veo una 'a', vuelvo a q1.
+  , ("q2", 'b', ["q0"])         -- Si ya terminé y veo una 'b', vuelvo a q0.
+  ]
+  ["q2"] 
