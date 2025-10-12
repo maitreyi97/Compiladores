d19ace433042280eebf69b4605c59c4362f9c798
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-10-12 15:36:21 -0600

    Finalizado sin Test

--- Proyecto1/src/AFD/AFD.hs
+++ Proyecto1/src/AFD/AFD.hs
@@ -33,7 +33,7 @@ buscarTransicion ((estadoInicial, caracter, estadoFinal):xs) estado simbolo
 minimizarAFD :: AFD -> AFD
 minimizarAFD (AFD estados alfabeto estadoInicial transiciones estadosFinales) =
   let pares = (generarTabla transiciones (revisarCeldas (generarPares(estados)) estadosFinales) alfabeto)
-      nuevosEstados = colapsarEstados pares
+      nuevosEstados = nub [encontrarRepresentante pares e | e <- estados]
       nuevasTransiciones = colapsarTransiciones nuevosEstados pares transiciones alfabeto
       nuevoEstadoInicial = encontrarRepresentante pares estadoInicial
       nuevoEstadoFinal = nub [encontrarRepresentante pares e | e <- estadosFinales] in
@@ -54,7 +54,7 @@ colapsarTransiciones nuevosEstados pares transicionesOriginales alfabeto =
                           let maybeDestinoOriginal = buscarTransicion transicionesOriginales nuevoEstado simbolo,
                           isJust maybeDestinoOriginal,
                           let destinoOriginal = quitarMaybe maybeDestinoOriginal,
-                          let nuevoDestino = encontrarRepresentante destinoOriginal pares
+                          let nuevoDestino = encontrarRepresentante pares destinoOriginal
                         ] in (nub nuevaTransicion)
 
 
@@ -64,18 +64,6 @@ encontrarRepresentante ((p, listaQs):resto) estado
   | estado == p || estado `elem` listaQs = p
   | otherwise = encontrarRepresentante resto estado
     
--- colapsarEstados :: Pares de Estados iguales -> Estados
-colapsarEstados :: [(String, [String])] -> [String]
-colapsarEstados [] = []
-colapsarEstados ((p, []):resto) = p:(colapsarEstados resto)
-colapsarEstados ((p, listaQs):resto) = let estadosObtenidos = (map (\x -> (removerEstado resto x)) listaQs) in
-  colapsarEstados ((p, estadosObtenidos):resto)
-
--- removerEstado :: pares con el estado a remover -> estado a remover -> estados iguales en el par removido
-removerEstado :: [(String, [String])] -> String -> [String]
-removerEstado [] estado = []
-removerEstado ((p, listaQs):resto) remover = if p == remover then listaQs else removerEstado resto remover
-
 -- generarPares :: Estados -> Pares de estados
 generarPares :: [String] -> [(String, [String])]
 generarPares [] = []
@@ -84,7 +72,7 @@ generarPares (estado:resto) = (estado,resto):(generarPares resto)
 -- revisarCeldas :: Pares de estados -> Estados Finales -> Pares de estados
 revisarCeldas :: [(String, [String])] -> [String] -> [(String, [String])]
 revisarCeldas [] finales = []
-revisarCeldas ((estado,pares):resto) finales  = (estado, (filter (\ s -> ( ((estado `elem` finales) && (not(s `elem` finales)))  || ((not (estado `elem` finales)) && (s `elem` finales)))) pares)):(revisarCeldas resto finales)   
+revisarCeldas ((estado,pares):resto) finales  = (estado, (filter (\ s -> ( ((estado `elem` finales) && ((s `elem` finales)))  || ((not (estado `elem` finales)) && ( not (s `elem` finales))))) pares)):(revisarCeldas resto finales)   
 
 -- revisarTransiciones :: Transiciones -> Pares de Estados -> Alfabeto -> ParesDeEstados
 revisarTransiciones :: [(String, Char, String)] -> [(String, [String])] -> [Char] -> [(String, [String])]
@@ -92,18 +80,16 @@ revisarTransiciones transiciones pares alfabeto = (map (\(x,y) -> (x, (revisarMa
 
 -- EstaMarcado :: Estado 1 -> Estado 2 -> Pares De Estados -> True si esta en pares de estados, false si no está
 estaMarcado :: String -> String -> [(String,[String])] -> Bool
-estaMarcado p q [] = False
+estaMarcado p q _ | p == q = False
+estaMarcado _ _ [] = True
 estaMarcado p q ((p1, listaQs):xs)
-  | p == p1 = not (q `elem` listaQs) || (estaMarcado p q xs)
-  | q == p1 = not (p `elem` listaQs) || (estaMarcado p q xs)
+  | p == p1 && (q `elem` listaQs) = False
+  | q == p1 && (p `elem` listaQs) = False
   | otherwise = estaMarcado p q xs
 
 -- revisarMarcadocontransicion :: Transiciones -> Pares -> Estado -> Lista de Estados Emparejados -> Alfabeto -> Nueva Lista de Estados Emparejados  
 revisarMarcadoConTransicion :: [(String, Char, String)] -> [(String, [String])] -> String -> [String] -> [Char] -> [String]
-revisarMarcadoConTransicion transiciones pares estado listaQs [] = listaQs
-revisarMarcadoConTransicion transiciones pares estado listaQs (a:alfabeto) =
-  let estadosTransicionados = (map (\x -> quitarMaybe (buscarTransicion transiciones x a)) listaQs) in
-    (filter (\q -> (estaMarcado (quitarMaybe (buscarTransicion transiciones estado a)) (quitarMaybe (buscarTransicion transiciones q a)) pares)) estadosTransicionados)
+revisarMarcadoConTransicion transiciones pares estado listaQs alfabeto = filter (\q -> (all (\a -> (not (estaMarcado (quitarMaybe (buscarTransicion transiciones estado a)) (quitarMaybe (buscarTransicion transiciones q a)) pares))) alfabeto)) listaQs
 
 -- quitarmaybe Maybe Estado -> Estado o Estado de Rechazo
 quitarMaybe :: Maybe String -> String
