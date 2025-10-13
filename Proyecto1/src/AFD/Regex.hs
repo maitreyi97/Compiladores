c198036dd051cc27331bbce4f210be9d69298004
Juvenal <38957852+JuvsProgramer@users.noreply.github.com>
2025-10-12 23:56:33 -0600

    Agrega implementacion de funcion para hacer match en regex.

--- Proyecto1/src/AFD/Regex.hs
+++ Proyecto1/src/AFD/Regex.hs
@@ -6,3 +6,24 @@ data RegEx = Epsilon
             | Concat RegEx RegEx
             | Star RegEx
             deriving Eq
+
+instance Show RegEx where 
+    show (Star r) = "(" ++ show r ++ ")*"
+    show (Concat r1 r2) = "(" ++ show r1 ++ show r2 ++ ")"
+    show (Or r1 r2) = "(" ++ show r1 ++ "|" ++ show r2 ++ ")"
+    show Epsilon = "ε"
+    show (Literal c) = [c]
+
+
+matchRegEx :: RegEx -> String -> Bool
+matchRegEx Epsilon c = c == ""
+matchRegEx (Literal x) c = c == [x]
+matchRegEx (Or i d) c = matchRegEx i c || matchRegEx d c
+matchRegEx (Concat i d) (c:cs) = matchRegEx i [c] && matchRegEx d cs
+matchRegEx (Star r) c = True
+    where
+        splits = allSplits c 
+
+        allSplits :: String -> [(String, String)]
+        allSplits st = [splitAt i st | i <- [0..length st]]
+
