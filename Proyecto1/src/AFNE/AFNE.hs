08865d02b528b1d842d150b89468f522976b04d6
rickivan <111657905+rickivan@users.noreply.github.com>
2025-10-16 08:18:54 -0600

    Cambiar transicionesE por deltaE para evitar errores

--- Proyecto1/src/AFNE/AFNE.hs
+++ Proyecto1/src/AFNE/AFNE.hs
@@ -7,14 +7,14 @@ data SimboloE = SimboloE Char | Eps deriving (Eq, Ord, Show)
 type Estado = String 
 type Estados = [Estado]
 type AlfabetoE = [SimboloE]
-type TransE = [(Estado, SimboloE, [Estado])]
+type DeltaE = [(Estado, SimboloE, [Estado])]
 type Inicial = Estado
 type Finales = [Estado]
 
 data AFNEp = AFNEp 
     { estados :: Estados,
      alfabetoE :: AlfabetoE,
-     transE :: TransE,
+     deltaE :: DeltaE,
      inicial :: Inicial,
      finales :: Finales
     } deriving (Show)
@@ -22,28 +22,27 @@ data AFNEp = AFNEp
 regexToAFNE :: RegEx -> AFNEp
 regexToAFNE Epsilon = AFNEp ["q0", "q1"] [Eps] [("q0", Eps, ["q1"])] "q0" ["q1"]
 regexToAFNE (Literal c) = AFNEp ["q0", "q1"] [SimboloE c] [("q0", SimboloE c, ["q1"])] "q0" ["q1"]
-regexToAFNE (Concat l r) = AFNEp estados alfabeto transiciones inicial finales
-  where AFNEp estadosL alfabetoL transicionesL inicialL finalesL = regexToAFNE l
-        AFNEp estadosR alfabetoR transicionesR inicialR finalesR = regexToAFNE r
+regexToAFNE (Concat l r) = AFNEp estados alfabeto delta inicial finales
+  where AFNEp estadosL alfabetoL deltaL inicialL finalesL = regexToAFNE l
+        AFNEp estadosR alfabetoR deltaR inicialR finalesR = regexToAFNE r
         estados = toList $ fromList (estadosL ++ estadosR)
         alfabeto = toList $ fromList (alfabetoL ++ alfabetoR)
-        transiciones = transicionesL ++ transicionesR ++ [(finalL, Eps, [inicialR]) | finalL <- finalesL] 
+        delta = deltaL ++ deltaR ++ [(finalL, Eps, [inicialR]) | finalL <- finalesL] 
         inicial = inicialL 
         finales = finalesR
-regexToAFNE (Or l r) = AFNEp estados alfabeto transiciones inicial finales
-  where AFNEp estadosL alfabetoL transicionesL inicialL finalesL = regexToAFNE l
-        AFNEp estadosR alfabetoR transicionesR inicialR finalesR = regexToAFNE r
+regexToAFNE (Or l r) = AFNEp estados alfabeto delta inicial finales
+  where AFNEp estadosL alfabetoL deltaL inicialL finalesL = regexToAFNE l
+        AFNEp estadosR alfabetoR deltaR inicialR finalesR = regexToAFNE r
         estados = toList $ fromList ("q_0" : estadosL ++ estadosR) 
         alfabeto = toList $ fromList (alfabetoL ++ alfabetoR) 
-        transiciones = transicionesL ++ transicionesR ++ [("q_0", Eps, [inicialL]), ("q_0", Eps, [inicialR])]
-        transiciones = transicionesL ++ transicionesR ++ [("q_0", Eps, [inicialL]), ("q_0", Eps, [inicialR])]
+        delta = deltaL ++ deltaR ++ [("q_0", Eps, [inicialL]), ("q_0", Eps, [inicialR])]
         inicial = "q_0"
         finales = finalesL ++ finalesR
-regexToAFNE (Star e) = AFNEp estados alfabeto transiciones inicial finales
-  where AFNEp estadosE alfabetoE transicionesE inicialE finalesE = regexToAFNE e
+regexToAFNE (Star e) = AFNEp estados alfabeto delta inicial finales
+  where AFNEp estadosE alfabetoE deltaE inicialE finalesE = regexToAFNE e
         estados = estadosE
         alfabeto = alfabetoE 
-        transiciones = transicionesE ++ [ (finalE, Eps, [inicialE]) | finalE <- finalesE ] 
+        delta = deltaE ++ [ (finalE, Eps, [inicialE]) | finalE <- finalesE ] 
         inicial = inicialE
         finales = inicialE : finalesE
         
