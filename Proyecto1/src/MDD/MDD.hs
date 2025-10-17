ef9c3c3ec27110ebd9d90186dfebe769fd154826
MilleniumParade <metzi.ar18@ciencias.unam.mx>
2025-10-17 13:06:01 -0600

    Carpeta para MDD

---
+++ Proyecto1/src/MDD/MDD.hs
@@ -0,0 +1,17 @@
+module MDD.MDD where
+import Regex.Regex
+import Regex.SpecReader
+
+type Estado = String
+type Simbolo = Char
+
+data MDD = MDD {
+    estados :: [Estado],
+    alfabeto :: [Simbolo],
+    transiciones :: [Estado, Simbolo, Estado],  
+    estadoInicial :: Estado,
+    finales :: [Estado],
+    mu :: [(Estado, Categoria)],             
+    errorLexico :: Categoria                
+} deriving (Show)
+  
