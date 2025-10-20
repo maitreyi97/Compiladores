ed1665166a2419d582df51126b6ccdd257dcaceb
Vidal Aguilar Diego Jesus <vidalaguilardiego@ciencias.unam.mx>
2025-10-19 22:34:23 -0600

    Documentacion

--- Proyecto1/src/AFN/AFN.hs
+++ Proyecto1/src/AFN/AFN.hs
@@ -11,21 +11,33 @@ data AFN = AFN
   [String] -- EstadosFinales
   deriving (Show)
 
+-- | Función encargada de revisar si una cadena es aceptada o no por nuestro AFN
+-- Realiza las transiciones desde el estado inicial usando la cadena hasta que ya no quede cadena. Una vez terminado se encarga de revisar si alguno de los estados alcanzados son parte del estado final, si lo son regresa true, si no lo son regresa false.
 aceptaAFN :: AFN -> String -> Bool
 aceptaAFN (AFN estados alfabeto estadoInicial transiciones estadosFinales) cadena =
   or (map (`elem` (transicionAFN transiciones [estadoInicial] cadena)) estadosFinales)
 
+-- | Función encargada de realizar las transiciones de un AFN correspondientes a una cadena proporcionada hasta que la cadena sea vacia, una vez que la cadena es vacia regresamos los estados alcanzados
+-- Usa la recursión para consumir caracter por caracter y realizar transiciones de tal manera que cada caracter que consumimos realizamos su correspondiente transicion para cada uno de los estados ingresados, concatenamos el resultado para que vuelva a ser una lista de estados y continuamos.
 transicionAFN :: [(String, Char, [String])] -> [String] -> String -> [String]
 transicionAFN _ estadoActual "" = estadoActual
 transicionAFN transiciones estados (x:xs) =
   transicionAFN transiciones (concat (map (\estadoActual -> auxiliarTransicion transiciones estadoActual x) estados)) xs
-  
+
+-- | Función encargada de realizar la transición dado un estado y un caracter revisa cual transición le corresponde y regresa la aplicación de esa transición.
 auxiliarTransicion :: [(String, Char, [String])] -> String -> Char -> [String]
 auxiliarTransicion [] _ _ = []
 auxiliarTransicion ((estadoInicial, caracter, estadoFinal):cs) estadoActual simbolo
   | estadoInicial == estadoActual && simbolo == caracter = estadoFinal ++ auxiliarTransicion cs estadoActual simbolo
   | otherwise = auxiliarTransicion cs estadoActual simbolo
 
+-- | Función encargada de transformar un AFN en AFD
+-- Primero crea el conjuntoPotencia de los estados
+-- Posteriormente crea la tabla de Transiciones sobre la cual se realiza el algoritmo para tranformar un AFN a unb AFN
+-- Usando el resultado de la función anterior, se revisa que estados son factibles en nuestro nuevo automata, para no tener estados a los que no se pueda acceder
+-- Posteriormente se revisa a partir de estos estados que estados son finales en el nuevo automata.
+-- Se limpian las transiciones para que podramos generar nuevas transiciones a partir de los estados factibles y la tabla de transiciones.
+-- Se formatean las cadenas de tal forma que formen un automata.
 
 afn_afd :: AFN -> AFD
 afn_afd (AFN estados alfabeto estadoInicial transiciones estadosFinales) =
@@ -40,7 +52,8 @@ afn_afd (AFN estados alfabeto estadoInicial transiciones estadosFinales) =
       in (AFD estadosFactiblesFormateados alfabeto estadoInicial transicionesFinalesFormateadas nuevosEstadosFinalesFormateados)
 
 
-
+-- | Función encargada de revisar que estados son alcanzables a partir de una lista de transiciones y un estado inicial.
+-- Prueba recursivamente los estados que son alcanzables, una vez que hemos alcanzado un nuevo estado se añade a la lista de estados alcanzados y posteriormente si ya no quedan estados por verificar se termina la función regresando todos los estados alcanzados. 
 -- estadosAlcanzables :: Transiciones -> EstadosActuales -> EstadosVistos -> EstadosAlcanzables
 estadosAlcanzables :: [([String], Char, [String])] -> [[String]] -> [[String]] -> [[String]]
 estadosAlcanzables _ [] alcanzados = alcanzados
@@ -49,21 +62,24 @@ estadosAlcanzables transiciones estadosActuales alcanzados =
       estadosNuevos = filter (\q -> (not (null q) && (not (q `elem` alcanzados)))) estadosSiguientes
   in estadosAlcanzables transiciones estadosNuevos (nub (estadosNuevos ++ alcanzados))
   
-
+--- | Funcion auxiliar de estados alcanzables, regresa los estados alcanzados a partir de un estado en particular. 
 estadoAlcanzableAux :: [([String], Char, [String])] -> [String] -> [[String]]
 estadoAlcanzableAux transiciones estado = nub (map (\(s,_,f) -> if s == estado then f else []) transiciones)
 
+-- | Regresa el conjunto potencia de los estados
 -- conjuntoPotencia :: Estados -> ConjuntoPotenciaDeLosEstados
 conjuntoPotencia :: [String] -> [[String]]
 conjuntoPotencia [] = [[]]
 conjuntoPotencia (estado:resto) = let restoPotencia = conjuntoPotencia(resto) in
   nub (restoPotencia ++ (map (\x -> estado:x) restoPotencia))
 
+-- | Función encargada de regresar las nuevas transiciones que pueden obtenerse haciendo uso de una función auxiliar para poder sacar las transiciones potencia de todo el alfabeto 
 -- transicionesPotencia :: Transiciones -> PotenciaDeEstados -> Alfabeto -> EstadosResultantes
 transicionesPotencia :: [(String, Char, [String])] -> [[String]] -> [Char] -> [([String], Char, [String])]
 transicionesPotencia _ _ [] = []
 transicionesPotencia transiciones estados (a:alfabeto) = transicionesPotenciaAux transiciones estados (a:[]) ++ transicionesPotencia transiciones estados alfabeto
 
+-- | Función encarga de dado un simbolo del alfabeto obtener todos los estados a los que se puede llegar para cada estado en la potencia de estados.
 -- transicionesPotenciaAux :: Transiciones -> PotenciaDeEstados -> Simbolo del alfabeto -> TransicionesResultantes 
 transicionesPotenciaAux :: [(String, Char, [String])] -> [[String]] -> [Char]  -> [([String], Char, [String])]
 transicionesPotenciaAux transiciones estados (a:xs) =
--- Proyecto1/src/AFNE/AFNE.hs
+++ Proyecto1/src/AFNE/AFNE.hs
@@ -62,6 +62,13 @@ transiciona (AFNE _ _ deltaE _ _) estado simbolo =
                              simboloTransicion == simbolo]
 
 -- AFNE to AFN
+-- | Función encargada de transformar un AFNE a un AFN
+-- La función consiste en la transformación de un AFNE a un AFN a través del proceso visto en la clase
+-- Dicho proceso consiste en la formación de las cerraduras de Epsilon, la cual corresponde a los estados alcanzables, esta cerradura  es la que se utiliza para poder construir las nuevas transiciones que consideran los estados alcanzables a través de la epsilon transición
+-- A través de transiciones epsilon, posteriormente estas transicionesEpsilon son filtradas para poder contener unicamente transiciones que no sean llevadas a cabo por algun epsilon
+-- Seguido de esto formateamos el alfabeto para que este formado por unicamente caracteres (Tal como corresponde en AFN)
+-- Posteriormente se generar los estados finales, verificando si alguno de los estados de la par de cerradura se encuentra en estado final.
+-- Finalmente con los conjuntos generados es que construimos el AFN.
 
 afne_to_afn :: AFNE -> AFN
 afne_to_afn afne =
@@ -71,6 +78,9 @@ afne_to_afn afne =
       alfabetoFormateado = [c | SimboloE c <- (alfabetoE afne)]
       estadosFinales = [q | (q, qs) <- cerradurasEpsilon, any (`elem` finales afne) qs]
   in AFN (estados afne) alfabetoFormateado (inicial afne) transicionesEpsilonFormateadas estadosFinales
+
+-- | Función encargada de obtener la cerradura epsilon de un estado.
+-- Dado un estado, recursivamente a través de las transiciones del afne, se encarga de buscar todos los estados alcanzables a traves de transiciones epsilon desde el estado actual, una vez que no se encuentre diferencia entre el los estados se regresan los estados obtenidos. en otro caso se continua en la recursión.
 
 eClosure :: AFNE -> Estados -> Estados
 eClosure afne estadosIniciales = closureAux estadosIniciales
@@ -83,10 +93,19 @@ eClosure afne estadosIniciales = closureAux estadosIniciales
       in
         if length proximo == length actuales then actuales else closureAux proximo
 
+-- | Función encargada de obtener la cerradura epsilon de todos los estados del afne y ponerlo en un par ordenado junto con el estado del cual pertenece la cerradura
 
 epsilonClosureAFNE :: AFNE -> [(Estado, [Estado])]
 epsilonClosureAFNE afne = map (\q -> (q, eClosure afne [q])) (estados afne)
 
+-- | Función encargada de realizar la nueva lista de transiciones que formarán parte del AFN, para ello utiliza una serie de funciones anidadas para poder realizar este proceso.
+-- Funciones las cuales enumeraré desde la primera en realizarse hasta la ultima.
+-- Por lo que la primer función en realizarse es la siguiente: (map (\p -> (transiciona afne p a)) qs) función la cual se encarga de realizar la transición de los estados que forman parte de la cerradura con el simbolo correspondiente del alfabeto
+-- Posteriormente al resultado se le hará un concat para hacer una lista de estados y finalmente eliminar los duplicados de esta lista de estados.
+-- después de esto se realiza el map anidado, el cual se encargará de que a cada uno de los estados resultantes buscar la cerradura epsilon correspendiente. Eso se realiza con la siguiente parte de la función (nub (concat ( map (\x -> getEpsilonClosure cerradurasEpsilon x) [parteAnterior] )) ) a este resultado es al que se le aplica un concat para convertirlo en una sola lista y un nub para quitar repeticiones.
+-- UNa vez hecho esto se realiza un map para cada uno de los elementos del alfabeto del afne se realizará lo anterior, elementos los cuales la salida será formateada como solicita la estructura DeltaE. esto se realiza en la siguiente función (map (\a -> (q,a, [partesAnteriores])) (alfabetoE afne) )
+-- Esto será realizado para cada estado devuelto en nuestra cerradura de Epsilon, pensando en no tener que capturar la cerradura dado el estado, porque al iterar sobre estos pares ya tenemos la cerradura y el estado del cual vienen.
+-- Finalmente filtraremos estas transiciones resultantes de tal forma que solo nos quedamos con las que no son vacias o no se componen de epsilon. 
 transicionesClosure :: AFNE -> [(Estado, [Estado])] -> DeltaE
 transicionesClosure afne cerradurasEpsilon =
   filter (\(estado, simbolo, estadosL) -> if simbolo == Eps || estadosL == [] then False  else True) (concat (map (\(q,qs) ->  (map (\a -> (q,a,(nub (concat ( map (\x -> getEpsilonClosure cerradurasEpsilon x) (nub (concat (map (\p -> (transiciona afne p a)) qs))) )) ))) (alfabetoE afne) ) ) cerradurasEpsilon))
